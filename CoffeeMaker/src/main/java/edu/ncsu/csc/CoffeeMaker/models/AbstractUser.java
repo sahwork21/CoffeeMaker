e989f0af64d86838f016885a03fe128e35c08d9e
sahinto2 <sahinto2@ncsu.edu>
2023-11-01 15:27:38 -0400

    [Skeleton] Created the class for the AbstractUser class

---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/AbstractUser.java
@@ -0,0 +1,65 @@
+package edu.ncsu.csc.CoffeeMaker.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import edu.ncsu.csc.CoffeeMaker.models.enums.Role;
+
+/**
+ * The Parent Class of all users that have an account in the system. Every user
+ * has a username, password, and Role Passwords should not be returned to the
+ * frontend with a JSON Roles determine what privileges the user has.
+ */
+
+@Entity
+@JsonIgnoreProperties ( value = { "password" } )
+public abstract class AbstractUser extends DomainObject {
+    /** User id */
+    @Id
+    @GeneratedValue
+    private Long         id;
+
+    /**
+     * Username for the user to identify who they are in the system
+     */
+    @Column ( name = "name" )
+    private final String username;
+
+    /**
+     * Password for the user to allow login with the username. This value should
+     * be hashed. Do not return it in the JSON files
+     */
+    private final String password;
+
+    /** The user's role and privileges */
+    private final Role   roleType;
+
+    /** Constructor empty constructor */
+    public AbstractUser () {
+        username = "";
+        password = "";
+        roleType = null;
+    }
+
+    /**
+     * Constructor to be used by subclasses to set all the fields
+     *
+     * @param username
+     *            the username of the user to identify them
+     * @param password
+     *            the password that we will hash then save
+     * @param roleType
+     *            the role of the user
+     */
+    public AbstractUser ( final String username, final String password, final Role roleType ) {
+
+        this.username = username;
+        this.password = password;
+        this.roleType = roleType;
+    }
+
+}
---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Customer.java
@@ -0,0 +1,24 @@
+package edu.ncsu.csc.CoffeeMaker.models;
+
+import javax.persistence.Entity;
+
+/**
+ * The class representation of the Customers in the system. Customers should be
+ * able to order recipes, view those orders, and pick them up. When they make an
+ * order an order object will be linked to this Customer in the database. When
+ * they pick up an order the order will be removed from their list and go to the
+ * history of all orders.
+ */
+@Entity
+public class Customer extends AbstractUser {
+
+    /**
+     * Returns the id of the user
+     */
+    @Override
+    public Long getId () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
