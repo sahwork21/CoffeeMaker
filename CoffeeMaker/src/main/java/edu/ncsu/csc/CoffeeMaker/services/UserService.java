ccdf6a51eaed976312d6b64bfc8aaf05c9488806
sahinto2 <sahinto2@ncsu.edu>
2023-11-02 10:16:38 -0400

    [Implementation] #28 Created the Repository and Service classes for Customer

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/services/UserService.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/services/UserService.java
@@ -13,16 +13,19 @@ import edu.ncsu.csc.CoffeeMaker.repositories.UserRepository;
  * Handler of the CRUD operations involving all users in the system. We have all
  * the functions of the Service class and the ability to find a user by
  * username.
+ *
+ * @param <E>
+ *            generic for the User class we want
  */
 @Component
 @Transactional
-public class UserService extends Service<AbstractUser, Long> {
+public class UserService <E extends AbstractUser> extends Service<AbstractUser, Long> {
 
     /**
      * Repository object that provides us the operations
      */
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository<E> userRepository;
 
     @Override
     protected JpaRepository<AbstractUser, Long> getRepository () {
