5fdd6da49659f9140a40eb50a433d42671af6022
Julian Madrigal <jmadrig@ncsu.edu>
2023-11-02 11:05:01 -0400

    [Frontend][Implementation][UC1] #20 Created main page for manager role

---
+++ CoffeeMaker/src/main/resources/templates/manager/manager.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp', []);
+
+
+app.controller('CustomerController', function($scope, $http, $q) {
+	$scope.actions = [
+		{title: "Add inventory", description: "Running low?", "icon": ("<svg></svg>")},
+		{title: "Add Recipe", description: "Have a creative idea?", "icon": null}
+	]
+	$scope.orders = [{id:1525, status:"In Progress", placedAt: "11:23 AM October 28, 2023"}, {id:5252, status:"In Progress", placedAt: "11:23 AM October 28, 2023"}];
+	
+	$scope.fetchOrders = function() {
+		$http.get("/api/v1/orders").then(function(response) {
+			$scope.orders = response.data;
+		});
+	}
+	
+});
