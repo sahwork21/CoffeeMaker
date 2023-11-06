a85162a756f1dda0978ed4a98a8f67ad211b2ea4
sahinto2 <sahinto2@ncsu.edu>
2023-11-05 22:52:19 -0500

    [Skeleton] Made skeleton of security filter chain for a login

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/configuration/SecSecurityConfig.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/configuration/SecSecurityConfig.java
@@ -1,28 +1,37 @@
 package edu.ncsu.csc.CoffeeMaker.configuration;
 
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
-//
-// @Configuration
-// @EnableWebSecurity
-// public class SecSecurityConfig {
-//
-// @Bean
-// public InMemoryUserDetailsManager userDetailsService () {
-// // InMemoryUserDetailsManager (see below)
-// }
-//
-// @Bean
-// public SecurityFilterChain filterChain ( final HttpSecurity http ) throws
-// Exception {
-// // http builder configurations for authorize requests and form login
-// // (see below)
-// }
-// }
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Manage the login flow of the system using a filterChain to restrict allowed
+ * inputs
+ */
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+
+    /**
+     * Build our security chain for the http request with the default page of
+     * signin and allowing the login API call
+     *
+     * @param http
+     *            the protocol builder we get to start
+     * @return a filter chain for the system to control how signins work
+     * @throws Exception
+     *             if the http is formatted incorrectly
+     */
+    @Bean
+    public SecurityFilterChain filterChain ( final HttpSecurity http ) throws Exception {
+        http.authorizeRequests().antMatchers( "/customer/**" ).hasRole( "CUSTOMER" ).antMatchers( "/barista/**" )
+                .hasRole( "BARISTA" ).antMatchers( "/manager/**" ).hasRole( "MANAGER" ).and().formLogin()
+                .loginPage( "/signin.html" ).loginProcessingUrl(
+                        "/login" )/*
+                                   * .defaultSuccessUrl( "/index.html", true )
+                                   */;
+        return http.build();
+    }
+}
