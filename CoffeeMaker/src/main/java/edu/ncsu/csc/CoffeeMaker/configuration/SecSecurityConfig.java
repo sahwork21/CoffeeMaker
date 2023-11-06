805eeea0e61af7dc89366eed08e66fdaff36ea71
sahinto2 <sahinto2@ncsu.edu>
2023-11-05 22:22:15 -0500

    [Fix] Added hacky fix to see index page on login

---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/configuration/SecSecurityConfig.java
@@ -0,0 +1,28 @@
+package edu.ncsu.csc.CoffeeMaker.configuration;
+
+//
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import
+// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+// import org.springframework.security.web.SecurityFilterChain;
+//
+// @Configuration
+// @EnableWebSecurity
+// public class SecSecurityConfig {
+//
+// @Bean
+// public InMemoryUserDetailsManager userDetailsService () {
+// // InMemoryUserDetailsManager (see below)
+// }
+//
+// @Bean
+// public SecurityFilterChain filterChain ( final HttpSecurity http ) throws
+// Exception {
+// // http builder configurations for authorize requests and form login
+// // (see below)
+// }
+// }
