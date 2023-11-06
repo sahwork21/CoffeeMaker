4e4ce4cae070c9918b1e23432ad800bd014934f4
sahinto2 <sahinto2@ncsu.edu>
2023-11-06 13:43:49 -0500

    [Implementation]#28 added find by role type to staff service class

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/repositories/StaffRepository.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/repositories/StaffRepository.java
@@ -1,6 +1,9 @@
 package edu.ncsu.csc.CoffeeMaker.repositories;
 
+import java.util.List;
+
 import edu.ncsu.csc.CoffeeMaker.models.Staff;
+import edu.ncsu.csc.CoffeeMaker.models.enums.Role;
 
 /**
  * Staff Repository for saving the Staff objects to the server and getting the
@@ -17,4 +20,13 @@ public interface StaffRepository extends UserRepository<Staff> {
      */
     @Override
     public Staff findByUsername ( final String username );
+
+    /**
+     * Returns a list of Staff members and only staff based on their user types
+     *
+     * @param roleType
+     *            the type of user we are looking for
+     * @return a list of Staff objects
+     */
+    public List<Staff> findByRoleType ( final Role roleType );
 }
