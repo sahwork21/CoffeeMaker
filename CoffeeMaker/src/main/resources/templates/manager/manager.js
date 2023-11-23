bb7415402b36390abfdc3a4c0ab111e35ac48bc9
Julian Madrigal <jmadrig@ncsu.edu>
2023-11-23 00:00:48 -0500

    [Implementation] Updated manager button references

--- CoffeeMaker/src/main/resources/templates/manager/manager.js
+++ CoffeeMaker/src/main/resources/templates/manager/manager.js
@@ -4,10 +4,10 @@ var app = angular.module('myApp', []);
 app.controller('ManagerController', function($scope, $http, $q) {
 	$scope.actions = [
 		{ title: "Add inventory", description: "Running low?", "icon": ("<svg></svg>"), href: "/inventory", color: "text-blue-500" },
-		{ title: "Add Recipe", description: "Have a creative idea?", "icon": null, href: "/addrecipe", color: "text-amber-500" },
+		{ title: "Add Recipe", description: "Have a creative idea?", "icon": null, href: "/addRecipe", color: "text-amber-500" },
 		{ title: "Edit Recipe", description: "Have a creative idea?", "icon": null, href: "/editrecipe", color: "text-indigo-500" },
-		{ title: "Delete Recipe", description: "Delete an existing recipe", "icon": null, href: "/deleterecipe", color: "text-green-700" },
-		{ title: "Add Ingredient", description: "Pumpkin spice season", "icon": null, href: "/ingredient", color: "text-yellow-500" },
+		{ title: "Delete Recipe", description: "Delete an existing recipe", "icon": null, href: "/deleteRecipe", color: "text-green-700" },
+		{ title: "Add Ingredient", description: "Pumpkin spice season", "icon": null, href: "/addIngredient", color: "text-yellow-500" },
 		{ title: "Manage Baristas", description: "Add new member", "icon": null, href: "/manageBaristas", color: "text-stone-500" },
 	]
 	
