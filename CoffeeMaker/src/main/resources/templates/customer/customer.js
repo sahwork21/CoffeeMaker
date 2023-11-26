62e5d510876f57f6b96b56108873832ae47fb754
jmadrig <jmadrig@ncsu.edu>
2023-11-26 15:39:11 -0500

    Revert "[Frontend][UC12] Completed Customer Pick Up webpage flow "

--- CoffeeMaker/src/main/resources/templates/customer/customer.js
+++ CoffeeMaker/src/main/resources/templates/customer/customer.js
@@ -1,17 +1,8 @@
 var app = angular.module('myApp', []);
 
 
-
-/**
- * TODO: Use polling to continuously fetch the status of orders.
- */
 app.controller('CustomerController', function($scope, $http, $q) {
-	$scope.orders = [{id:1525, status:"In Progress", placedAt: "11:23 AM October 28, 2023"}, {id:5252, status:"Ready for pickup", placedAt: "11:23 AM October 28, 2023"}];
-	
-	$scope.onOrderPickup = function(order) {
-		// For testing, removes a order from pickup
-		$scope.orders = $scope.orders.filter(anOrder => anOrder !== order);
-	}
+	$scope.orders = [{id:1525, status:"In Progress", placedAt: "11:23 AM October 28, 2023"}, {id:5252, status:"In Progress", placedAt: "11:23 AM October 28, 2023"}];
 	
 	$scope.fetchOrders = function() {
 		$http.get("/api/v1/orders").then(function(response) {
