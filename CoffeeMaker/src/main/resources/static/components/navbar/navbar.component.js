a26e1a8e1e5f804c9ebe571ad65f0477fe6a7beb
Julian Madrigal <jmadrig@ncsu.edu>
2023-11-26 11:08:44 -0500

    [Implementation] Updated navbar and included log out #66

--- CoffeeMaker/src/main/resources/static/components/navbar/navbar.component.js
+++ CoffeeMaker/src/main/resources/static/components/navbar/navbar.component.js
@@ -1,7 +1,42 @@
 
-angular.module("myApp").component("navbar", {
-	templateUrl: "/components/navbar/navbar.template.html",
-	controller: function NavBarController() {
-		this.user="Julian"
+
+function NavBarController($scope, $element, $attrs) {
+	var ctrl = this;
+	ctrl.username = sessionStorage.getItem("username");
+	ctrl.userRole = sessionStorage.getItem("userRole");
+	console.log(ctrl.userRole)
+	
+	if (ctrl.userRole == null) {
+		ctrl.username = ""
 	}
-})
+	
+	
+	ctrl.signOut = function() {
+		sessionStorage.setItem("username", null);
+		sessionStorage.setItem("userRole", null);
+		location.href = "signin.html";
+	}
+	
+	ctrl.goToMain = function() {
+		redirects = {
+			"CUSTOMER": "customer.html",
+			"MANAGER": "manager.html",
+			"BARISTA": "barista.html"
+		}
+		
+		console.log(redirects[ctrl.userRole])
+		
+		if (redirects[ctrl.userRole]) {
+			location.href = "../../" + redirects[ctrl.userRole]
+		} else {
+			location.href = "signin.html"
+		}
+		
+	}
+}
+
+angular.module('myApp').component('navbar', {
+	templateUrl: '/components/navbar/navbar.template.html',
+	controller: NavBarController
+});
+
